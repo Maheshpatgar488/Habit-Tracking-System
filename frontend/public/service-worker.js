ae9f61453b6f0a458c0a2eacd50e1d06a60f110c
Mahesh Shridhar  Patgar <maheshpatgar488@gmail.com>
2026-05-17 09:36:47 +0530

    Fix notification click URL to be relative

--- frontend/public/service-worker.js
+++ frontend/public/service-worker.js
@@ -27,6 +27,6 @@ self.addEventListener('notificationclick', function(event) {
   event.notification.close();
 
   event.waitUntil(
-    clients.openWindow('http://localhost:5173/user')
+    clients.openWindow('/user')
   );
 });
